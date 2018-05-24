5f1e515f87eb96c7fc52a73a4adbdb643d8e7d83
Nagabhushan <nagabhushan2222@gmail.com>
2018-05-24 16:21:53 +0530

    FadeAnimation

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,75 @@ class SampleApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new SampleAppPage(),
+      //home: new SampleAppPage(),
+      home: new MyFadeTest(),
     );
   }
 }
+
+class MyFadeTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Fade Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyFadeTestApp(title: 'Fade Demo'),
+    );
+  }
+}
+
+class MyFadeTestApp extends StatefulWidget {
+  final title;
+
+  MyFadeTestApp({Key key, this.title}) : super(key: key);
+
+  @override
+  _MyFadeTestAppState createState() => new _MyFadeTestAppState();
+}
+
+class _MyFadeTestAppState extends State<MyFadeTestApp>
+    with TickerProviderStateMixin {
+  AnimationController controller;
+  CurvedAnimation curvedAnimation;
+
+  @override
+  void initState() {
+    controller = new AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+    curvedAnimation =
+        new CurvedAnimation(parent: controller, curve: Curves.bounceIn);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+          child: new Container(
+        child: new FadeTransition(
+          opacity: curvedAnimation,
+          child: new FlutterLogo(
+            size: 100.0,
+          ),
+        ),
+      )),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () {
+          controller.forward();
+        },
+        child: new Icon(Icons.brush),
+        tooltip: 'Fade',
+      ),
+    );
+  }
+}
+
+/*
+* Toggle */
 
 class SampleAppPage extends StatefulWidget {
   SampleAppPage({Key key}) : super(key: key);
@@ -26,6 +91,7 @@ class SampleAppPage extends StatefulWidget {
 class _SampleAppPageState extends State<SampleAppPage> {
   // Default value for toggle
   bool toggle = true;
+
   void _toggle() {
     setState(() {
       toggle = !toggle;
@@ -36,7 +102,8 @@ class _SampleAppPageState extends State<SampleAppPage> {
     if (toggle) {
       return new Text('Toggle One');
     } else {
-      return new MaterialButton(onPressed: () {}, child: new Text('Toggle Two'));
+      return new MaterialButton(
+          onPressed: () {}, child: new Text('Toggle Two'));
     }
   }
 
