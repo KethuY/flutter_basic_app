8a2189b929b1d84f53f7d5d4cbd809bd50358b58
Nagabhushan <nagabhushan2222@gmail.com>
2018-05-24 16:30:34 +0530

    Sinature Painter

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,78 @@ class SampleApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       //home: new SampleAppPage(),
-      home: new MyFadeTest(),
+    //  home: new MyFadeTest(),
+      home: new DemoApp(),
     );
   }
 }
 
+
+class SignaturePainter extends CustomPainter{
+  final List<Offset> points;
+
+  SignaturePainter(this.points);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = new Paint()
+      ..color = Colors.black
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5.0;
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null)
+        canvas.drawLine(points[i], points[i + 1], paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(SignaturePainter other) => other.points != points;
+
+}
+class Signature extends StatefulWidget {
+  SignatureState createState() => new SignatureState();
+}
+class SignatureState extends State<Signature> {
+  List<Offset> _points = <Offset>[];
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onPanUpdate: (DragUpdateDetails details) {
+        setState(() {
+          RenderBox referenceBox = context.findRenderObject();
+          Offset localPosition =
+          referenceBox.globalToLocal(details.globalPosition);
+          _points = new List.from(_points)..add(localPosition);
+        });
+      },
+      onPanEnd: (DragEndDetails details) => _points.add(null),
+      child: new CustomPaint(painter: new SignaturePainter(_points), size: Size.infinite),
+    );
+  }
+}
+class DemoApp extends StatelessWidget {
+  Widget build(BuildContext context) => new Scaffold(body: new Signature());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Animation*/
 class MyFadeTest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
